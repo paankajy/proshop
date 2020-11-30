d3779af10304302d51d52e54f62fc49907ff5a51
pankaj <paankajy@gmail.com>
2020-11-30 21:22:49 +0530

    second

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,24 @@
-import './App.css';
 import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Header from './component/Header';
+import Footer from './component/Footer';
+import HomeScreen from './screens/HomeScreen';
+import { Container } from 'react-bootstrap';
+import ProductScreen from './screens/ProductScreen';
 
 const App = () => {
-  return <Fragment className='App'></Fragment>;
+  return (
+    <Router>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Route path='/' component={HomeScreen} exact />
+          <Route path='/product/:id' component={ProductScreen} />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
+  );
 };
 
 export default App;
